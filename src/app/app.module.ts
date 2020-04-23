35d0875ad93fabc81d39ca05a9878b2316bf76ca
Jamesson Faria <jamessonjr@gmail.com>
2020-04-23 20:42:24 -0300

    create components home, about and header, add routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,24 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
-
-import { AppComponent } from './app.component'
+import { ROUTES } from './app.routes';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component'
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
-    RouterModule
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
