c3be56017c720b781b465d681dfcc3d6053c6b40
Jamesson Faria <jamessonjr@gmail.com>
2020-04-23 21:17:00 -0300

    add restaurants and restaurant components, change router file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from "@angular/router";
 
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
+import { RestaurantsComponent } from './restaurants/restaurants.component';
 
 export const ROUTES: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'about', component: AboutComponent }
+    { path: 'about', component: AboutComponent },
+    { path: 'restaurants', component: RestaurantsComponent }
 ]
---
+++ src/app/restaurants/restaurants.component.ts
@@ -0,0 +1,34 @@
+import { Restaurant } from './restaurant/restaurant.model';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mt-restaurants',
+  templateUrl: './restaurants.component.html'
+})
+export class RestaurantsComponent implements OnInit {
+
+  restaurants: Restaurant[] = [
+    {
+      id: "bread-bakery",
+      name: "Bread & Bakery",
+      category: "Bakery",
+      deliveryEstimate: "25m",
+      rating: 4.9,
+      imagePath: "assets/img/restaurants/breadbakery.png",
+    },
+    {
+      id: "burger-house",
+      name: "Burger House",
+      category: "Hamburgers",
+      deliveryEstimate: "100m",
+      rating: 3.5,
+      imagePath: "assets/img/restaurants/burgerhouse.png",
+    },
+  ]
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
