23c58037e7e7a874f38bab565d646558dd943f85
Jamesson Faria <jamessonjr@gmail.com>
2020-05-05 20:38:40 -0300

    add service to check order

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,7 +1,9 @@
+import { Component, OnInit } from '@angular/core';
+
 import { CartItem } from './../restaurant-detail/shopping-cart/cart-item.model';
 import { OrderService } from './order.service';
 import { RadioOption } from './../shared/radio/radio-option.model';
-import { Component, OnInit } from '@angular/core';
+import { Order, OrderItem } from './order.model';
 
 @Component({
   selector: 'mt-order',
@@ -41,6 +43,16 @@ export class OrderComponent implements OnInit {
 
   remove(item: CartItem){
     this.orderService.remove(item)
+  }
+
+  checkOrder(order: Order){
+    order.orderItems = this.cartItems()
+      .map((item: CartItem) => new OrderItem(item.quantity, item.menuItem.id))
+    this.orderService.checkOrder(order).subscribe((orderId: string) => {
+      console.log(`Compra concluida: ${orderId}`)
+      this.orderService.clear()
+    })
+    console.log(order)
   }
 
 }
--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -1,13 +1,19 @@
+import { Http, Headers, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
 import { Injectable } from '@angular/core'
+import 'rxjs/add/operator/map'
 
 import { CartItem } from './../restaurant-detail/shopping-cart/cart-item.model';
 import { ShoppingCartService } from 'app/restaurant-detail/shopping-cart/shopping-cart.service';
+import { Order } from './order.model';
+import { MEAT_API } from './../app.api';
 
 @Injectable()
 export class OrderService {
-    
+
     constructor(
-        private cartService: ShoppingCartService
+        private cartService: ShoppingCartService,
+        private http: Http
     ){}
 
     itemsValue() {
@@ -16,6 +22,19 @@ export class OrderService {
 
     cartItems(): CartItem[] {
         return this.cartService.items
+    }
+
+    checkOrder(order: Order): Observable<string> {
+        const headers = new Headers()
+        headers.append('Content-Type', 'application/json')
+        return this.http.post(`${MEAT_API}/orders`,
+                              JSON.stringify(order),
+                              new RequestOptions({headers: headers}))
+                        .map(response=> response.json())
+    }
+
+    clear(){
+        this.cartService.clear()
     }
 
     increaseQty(item: CartItem){
