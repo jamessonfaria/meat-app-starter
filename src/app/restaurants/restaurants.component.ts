684e12e0a6e6ce31a1c06f7c71bf399d7483acad
Jamesson Faria <jamessonjr@gmail.com>
2020-05-10 18:22:50 -0300

    add debounceTime and distinctUntilChanged for search

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -4,6 +4,9 @@ import { Restaurant } from './restaurant/restaurant.model';
 import { Component, OnInit } from '@angular/core';
 import { trigger, state, style, transition, animate } from '@angular/animations'
 import 'rxjs/add/operator/switchMap'
+import 'rxjs/add/operator/do'
+import 'rxjs/add/operator/debounceTime'
+import 'rxjs/add/operator/distinctUntilChanged'
 
 @Component({
   selector: 'mt-restaurants',
@@ -42,6 +45,8 @@ export class RestaurantsComponent implements OnInit {
     })
 
     this.searchControl.valueChanges
+        .debounceTime(500)
+        .distinctUntilChanged()
         .switchMap(searchTerm => 
           this.restaurantsService.restaurants(searchTerm))
           .subscribe(restaurants => this.restaurants = restaurants)
