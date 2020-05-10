ccace6bccb22780eb13cc74e3acbefad90a1c0e7
Jamesson Faria <jamessonjr@gmail.com>
2020-05-10 20:10:29 -0300

    Adjust to add Observable.from catch error in observable

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -7,6 +7,9 @@ import 'rxjs/add/operator/switchMap'
 import 'rxjs/add/operator/do'
 import 'rxjs/add/operator/debounceTime'
 import 'rxjs/add/operator/distinctUntilChanged'
+import 'rxjs/add/operator/catch'
+import 'rxjs/add/observable/from'
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'mt-restaurants',
@@ -48,7 +51,9 @@ export class RestaurantsComponent implements OnInit {
         .debounceTime(500)
         .distinctUntilChanged()
         .switchMap(searchTerm => 
-          this.restaurantsService.restaurants(searchTerm))
+          this.restaurantsService
+          .restaurants(searchTerm)
+          .catch(error => Observable.from([])))
           .subscribe(restaurants => this.restaurants = restaurants)
         
     this.restaurantsService.restaurants()
