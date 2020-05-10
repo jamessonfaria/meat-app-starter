178681042c7ecc57a9f535c30a0afbdf90fb4b14
Jamesson Faria <jamessonjr@gmail.com>
2020-05-10 17:49:22 -0300

    add funcionality to search bar

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -1,13 +1,29 @@
 import { RestaurantService } from './restaurants.service';
 import { Restaurant } from './restaurant/restaurant.model';
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations'
 
 @Component({
   selector: 'mt-restaurants',
-  templateUrl: './restaurants.component.html'
+  templateUrl: './restaurants.component.html',
+  animations: [
+    trigger('toggleSearch', [
+      state('hidden', style({
+        opacity: 0,
+        "max-height" : "0px"
+      })),
+      state('visible', style({
+        opacity: 1,
+        "max-height" : "70px",
+        "margin-top" : "20px"
+      })),
+      transition('* => *', animate('250ms 0s ease-in-out'))
+    ])
+  ]
 })
 export class RestaurantsComponent implements OnInit {
 
+  searchBarState = 'hidden'
   restaurants: Restaurant[]
 
   constructor(private restaurantsService: RestaurantService) {}
@@ -15,6 +31,10 @@ export class RestaurantsComponent implements OnInit {
   ngOnInit() {
     this.restaurantsService.restaurants()
       .subscribe(rest => this.restaurants = rest)
+  }
+
+  toggleSearch(){
+    this.searchBarState = this.searchBarState === 'hidden' ? 'visible' : 'hidden'
   }
 
 }
