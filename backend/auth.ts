e84120e2eccbcaeed59e6a429812c92a470235b4
Jamesson Faria <jamessonjr@gmail.com>
2020-05-11 21:56:21 -0300

    add jwt in backend

--- backend/auth.ts
+++ backend/auth.ts
@@ -1,11 +1,17 @@
 import { Request, Response} from 'express'
 import { User, users } from './users'
 
+import * as jwt from 'jsonwebtoken'
+
 export const handleAuthentication = (req: Request, resp: Response) => {
     const user: User = req.body
     if(isValid(user)){
         const dbUser: User = users[user.email]
-        resp.status(200).json({name: dbUser.name, email: dbUser.email})
+        const token = jwt.sign({
+            sub: dbUser.email, 
+            iss: 'meat-api'}, 'meat-api-password')
+        
+        resp.json({name: dbUser.name, email: dbUser.email, accessToken: token})
     }else{
         resp.status(403).json({message: 'Dados inválidos.'})
     }
