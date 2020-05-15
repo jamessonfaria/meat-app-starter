d844ee966c4548c557867824a6e23ca7789ce0c7
Jamesson Faria <jamessonjr@gmail.com>
2020-05-14 22:37:20 -0300

    Adjust preserveWhiteSpaces

--- src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
+++ src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
@@ -6,6 +6,7 @@ import { ShoppingCartService } from './shopping-cart.service';
 @Component({
   selector: 'mt-shopping-cart',
   templateUrl: './shopping-cart.component.html',
+  preserveWhitespaces: true,
   animations: [
     trigger('row', [
       state('ready', style({opacity: 1})),
